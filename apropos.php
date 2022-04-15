4165f2e4d22b2020ad4cd7b39dfadcdf4d829f5d
alexmrs <almouras@ensc.fr>
2022-04-15 14:26:13 +0200

    Texte du a propos
    Ajout de texte dans à propos

--- apropos.php
+++ apropos.php
@@ -4,16 +4,18 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet">
         <title>À propos</title>
     </head>
 
 <?php include "header.php"; ?>
 
     <body>
-    <h1>À Propos</h1>
-    
-    <p>Ce site a été créé dans le cadre du projet de Communication Web de l'ENSC. Il a été réalisé par Alexandra MOURAS et Noah RICHERT.</p>
-    <p>ENSC 2022</p>
+    <div class="apropos">
+        <h1>À propos</h1>
+        <br> <br>
+        <p>Ce site a été créé dans le cadre du projet de Communication Web de l'ENSC. Il a été réalisé par Alexandra MOURAS et Noah RICHERT.</p> <p>ENSC 1A Groupe 4 - 2021/2022</p>
+    </div>
     </body>
 
 <?php include "footer.php"; ?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
         <title>StoryTime </title>
         <link href="moz-extension://6de86387-081d-400c-96ba-2e32fcf69c81/styles/host.css" rel="stylesheet">
     </head>
