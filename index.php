d207fbdad1bb87b278d08011ebd71c14b875d74e
NoR47000 <94907401+NoR47000@users.noreply.github.com>
2022-04-15 16:03:38 +0200

    clean
    clean

--- index.php
+++ index.php
@@ -4,16 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< Updated upstream
         
         <!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 		
         <link href="style.css" rel="stylesheet">
-=======
-        <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
->>>>>>> Stashed changes
+
         <title>StoryTime </title>
         <link href="moz-extension://6de86387-081d-400c-96ba-2e32fcf69c81/styles/host.css" rel="stylesheet">
     </head>
