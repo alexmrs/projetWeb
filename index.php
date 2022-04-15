8d996187ef395175a97168cba839f1b8558d540e
NoR47000 <94907401+NoR47000@users.noreply.github.com>
2022-04-15 13:46:01 +0200

    test1
    test1 html index

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <title>StoryTime </title>
+        <link href="moz-extension://6de86387-081d-400c-96ba-2e32fcf69c81/styles/host.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
 
-echo "coucou ";
-
-?>
+</html>
