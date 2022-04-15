807ee5ddbdd2d0d554d4b0649d580b8b583a18f2
alexmrs <almouras@ensc.fr>
2022-04-15 14:05:02 +0200

    A propos + footer
    Nouveau fichier a propos

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
-<div id="liveAlertPlaceholder"></div>
-<button type="button" class="btn btn-primary" id="liveAlertBtn">Show live alert</button>
+<footer class="container-fluid">
+		<nav class="navbar fixed-bottom navbar-light bg-light">
+  			<div class="container-fluid">
+   				 <a class="navbar-brand" href="apropos.php">À propos</a>
+  			</div>
+		</nav>
+</footer>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
+</html>
