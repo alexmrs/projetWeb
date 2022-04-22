c1bd72b9ab6e00bf69e82ccfe216d4ea801769dd
alexmrs <almouras@ensc.fr>
2022-04-22 10:54:02 +0200

    Ajout inscription
    inscription + admin dans le nav

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
         <img src="images/icones/iconeHistoire.jpg" alt="image Histoire" width="30px" height="30px">
         StoryTime
     </a>
+
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item dropdown">
@@ -17,20 +18,27 @@
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Administrateur</a>
       </ul>
     </div>
+
     <div class="navbar-right">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        <span class="glyphicon glyphicon-user"></span> Non connecté <b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="login.php">Se connecter</a></li>
-                    </ul>
-                </li>
-            </ul>
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Non connecté
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="connexion.php">Se connecter</a></li>
+            <li><a class="dropdown-item" href="inscription.php">Inscription</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Administrateur</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
+
   </div>
   
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
