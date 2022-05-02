92de7dc3834e1ef92a74b9f00d9789928454e916
alexmrs <almouras@ensc.fr>
2022-05-02 19:39:05 +0200

    déconnexion
    déconnexion accessible de partout

--- connexion.php
+++ connexion.php
@@ -80,8 +80,6 @@ $titrePage = "Connexion";
 
         <?php require_once "includes/footer.php"; ?>
    
-    <!-- Bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
 </body>
 
--- inscription.php
+++ inscription.php
@@ -50,8 +50,6 @@ require_once("includes/connect.php");
 
     <?php include "includes/footer.php"; ?>
 
-    <!-- Bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     </body>
 </html>
